78e386e2c29e7d6b9d20b9b3bb7e203d601ab025
Nahuel <>
2018-11-24 10:38:57 +0000

    Kata6 v1.0

---
+++ src/kata6/Kata6.java
@@ -0,0 +1,66 @@
+package kata6;
+
+import java.io.File;
+import java.util.Iterator;
+
+/**
+ * ITERADOR: RECORRE LA SECUENCIA DE ELEMENTOS UNA SOLA VEZ
+ * ITERABLE: PERMITE CONCEDER ITERADORES VARIAS VECES
+ */
+
+public class Kata6 {
+
+    public static void main(String[] args) {
+        File root = new File("/");
+        File[] files = root.listFiles();
+        new Histogram<>(megabytes(lengths(iteratorOf(files))));
+        
+    }
+    
+    private static <T> Iterator<T> iteratorOf (T[] items){
+        return new Iterator<T>() {
+            int index = 0;
+
+            @Override
+            public boolean hasNext() {
+                return index < items.length;
+            }
+
+            @Override
+            public T next() {
+                return items[index++];
+            }
+        };
+    }
+    
+    private static Iterator<Long> lengths (Iterator<File> iteratorOf) {
+        return new Iterator<Long>() {
+            @Override
+            public boolean hasNext() {
+                return iteratorOf.hasNext();
+            }
+
+            @Override
+            public Long next() {
+                return iteratorOf.next().length();
+            }
+        };
+    }
+    
+    private static Iterator<Integer> megabytes (Iterator<Long> lengths) {
+       return new Iterator<Integer>() {
+           @Override
+           public boolean hasNext() {
+               return lengths.hasNext();
+           }
+
+           @Override
+           public Integer next() {
+               return (int) (lengths.next()/1024*1024);
+           }
+       };
+    }
+    
+    
+    
+}
